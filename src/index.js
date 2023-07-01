e6cf90d79f8758d9e8ad26e31fc7560100e8c422
rmiyoyo <miyoyoraphael@gmail.com>
2023-07-01 17:43:22 +0300

    Implement refresh and submit actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css';
+import {
+  updatePoint, loadPoints, resetForm, warningReport,
+} from './modules/apiService.js';
+
+document.querySelector('.refresh').addEventListener('click', (event) => {
+  event.preventDefault();
+  loadPoints();
+});
+
+document.querySelector('.submit').addEventListener('click', (event) => {
+  event.preventDefault();
+  warningReport();
+  const playerName = document.querySelector('#player-name').value;
+  const playerPoints = document.querySelector('#player-score').value;
+
+  updatePoint(playerName, playerPoints);
+  resetForm();
+});
